badfc5dc9702ce21edd362bf5506e61a6b0e9625
ShantanuTaro <shantanutaro@gmail.com>
2025-07-05 21:41:24 +0530

    Initial Commit

---
+++ DailyMotivationTests/DailyMotivationTests.swift
@@ -0,0 +1,17 @@
+//
+//  DailyMotivationTests.swift
+//  DailyMotivationTests
+//
+//  Created by Shantanu Taro on 05/07/25.
+//
+
+import Testing
+@testable import DailyMotivation
+
+struct DailyMotivationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
